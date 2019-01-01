04916de68b901d12f53e55570d4a6f192cfc348b
Reinaldo Schiehll <rn.schiehll@gmail.com>
2019-01-01 06:07:27 -0200

    Add preload to screen routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import React, { Suspense } from 'react'
 import { Router } from '@reach/router'
-import LazyLoadScreen from 'utils/LazyLoadScreen'
+import lazyWithPreload from 'utils/lazyWithPreload'
 import Loader from 'components/loader'
 
 export const PATHS = {
   HOME: '/',
+  DETAILS: '/details',
   NO_MATCH: '*'
 }
 
@@ -14,14 +15,26 @@ const screens = preval`
   module.exports = fs.readdirSync(path.resolve(__dirname, '../screens'))
 `
 
-const Routes = () => (
-  <Suspense fallback={<Loader />}>
-    <Router>
-      {screens.map(screen => {
-        const Screen = LazyLoadScreen(screen)
-        const path = PATHS[screen.replace(/-/, '_').toUpperCase()]
+export const screenRoutes = screens.reduce((screenRoutes, screen) => {
+  const Screen = lazyWithPreload(() =>
+    import(/* webpackChunkName: "screen-[request]" */ `../screens/${screen}`)
+  )
 
-        if (!path) return null
+  const path = PATHS[screen.replace(/-/, '_').toUpperCase()]
+
+  if (!path) return screenRoutes
+
+  return screenRoutes.concat({
+    screen,
+    path,
+    component: Screen
+  })
+}, [])
+
+const Routes = () => (
+  <Suspense maxDuration={500} fallback={<Loader />}>
+    <Router>
+      {screenRoutes.map(({ screen, path, component: Screen }) => {
         return <Screen key={screen} path={path} default={path === '*'} />
       })}
     </Router>
--- src/screens/no-match/index.js
+++ src/screens/no-match/index.js
@@ -1,12 +1,22 @@
-import React, { Fragment } from 'react'
+import React, { useEffect, Fragment } from 'react'
 import { Link } from '@reach/router'
-import { PATHS } from 'routes'
+import { PATHS, screenRoutes } from 'routes'
 
-const NoMatch = () => (
-  <Fragment>
-    <h1>No Match</h1>
-    <Link to={PATHS.HOME}>home</Link>
-  </Fragment>
+const homeScreen = screenRoutes.find(
+  screenRoute => screenRoute.path === PATHS.HOME
 )
+
+const NoMatch = () => {
+  useEffect(() => {
+    if (homeScreen) homeScreen.component.preload()
+  }, [])
+
+  return (
+    <Fragment>
+      <h1>No Match</h1>
+      <Link to={PATHS.HOME}>home</Link>
+    </Fragment>
+  )
+}
 
 export default NoMatch
